eafc0e9b819384b45c01d9aa56910ac3c37f6252
victorngure <victorngure@gmail.com>
2020-11-05 21:55:26 +0300

    cdn updates

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     
     <link href="{{ asset('//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') }}" rel="stylesheet">
 
-    <link href="/public/css/sb-admin-2.css" rel="stylesheet">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/4.1.2/css/sb-admin-2.css" rel="stylesheet">
 
     <link href="{{ asset('//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css') }}" rel="stylesheet">
 
