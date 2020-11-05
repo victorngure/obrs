04d35a5bd95f9ba56fe3ff54876e2d16355478c0
victorngure <victorngure@gmail.com>
2020-11-05 22:27:51 +0300

    added master to auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     
     <link href="{{ asset('//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') }}" rel="stylesheet">
 
-    <link href="/public/css/sb-admin-2.css" rel="stylesheet">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/4.1.2/css/sb-admin-2.css" rel="stylesheet">
 
     <link href="{{ asset('//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css') }}" rel="stylesheet">
 
@@ -24,7 +24,7 @@
 
     <link href="//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap2.min.css" rel="stylesheet">
 
-    <link href="/public//css/style.css" rel="stylesheet">
+    <link href="{{ asset('/public//css/style.css') }}" rel="stylesheet">
 
 </head>
 <body>
